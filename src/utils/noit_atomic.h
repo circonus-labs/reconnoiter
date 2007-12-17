1452e3302fd8c2b4ded895773bf38dc163b2d914
Theo Schlossnagle <jesus@omniti.com>
2007-12-17 18:39:37 +0000

    add some x86 and x86_64 atomic support

--- src/utils/noit_atomic.h
+++ src/utils/noit_atomic.h
@@ -18,15 +18,74 @@ typedef int64_t noit_atomic64_t;
  */
 #include <libkern/OSAtomic.h>
 typedef OSSpinLock noit_spinlock_t;
-#define noit_atomic_cas OSAtomicCompareAndSwap32
+#define noit_atomic_cas32 OSAtomicCompareAndSwap32
 #define noit_atomic_cas64 OSAtomicCompareAndSwap64
-#define noit_atomic_inc OSAtomicIncrement32
+#define noit_atomic_inc32 OSAtomicIncrement32
 #define noit_atomic_inc64 OSAtomicIncrement64
-#define noit_atomic_dec OSAtomicDecrement32
+#define noit_atomic_dec32 OSAtomicDecrement32
 #define noit_atomic_dec64 OSAtomicDecrement64
 #define noit_spinlock_lock OSSpinLockLock
 #define noit_spinlock_unlock OSSpinLockUnlock
 #define noit_spinlock_trylock OSSpinLockTry
+#elif defined(__GNUC__)
+
+typedef u_int32_t noit_atomic32_t
+typedef u_int64_t noit_atomic64_t
+
+#if (SIZEOF_VOID_P == 4)
+#define noit_atomic_casptr(a,b,c) noit_atomic_cas32((a),(void *)(b),(void *)(c))
+#elif (SIZEOF_VOID_P == 8)
+#define noit_atomic_casptr(a,b,c) noit_atomic_cas64((a),(void *)(b),(void *)(c))
+#else
+#error unsupported pointer width
+#endif
+
+static inline noit_atomic32_t
+noit_atomic_cas32(volatile noit_atomic32_t *ptr
+                  volatile noit_atomic32_t rpl,
+                  volatile noit_atomic32_t curr) {
+  noit_atomic32_t prev;
+  __asm__ volatile (
+      "lock; cmpxchgl %1, %2"
+    : "=a" (prev)
+    : "r"  (rpl), "m" (*(ptr)), "0" (curr)
+    : "memory");
+  return prev;
+}
+
+#ifdef __x86_64__
+static inline noit_atomic64_t
+noit_atomic_cas64(volatile noit_atomic64_t *ptr
+                  volatile noit_atomic64_t rpl,
+                  volatile noit_atomic64_t curr) {
+  noit_atomic64_t prev;
+  __asm__ volatile (
+      "lock; cmpxchgq %1, %2"
+    : "=a" (prev)
+    : "r"  (rpl), "m" (*(ptr)), "0" (curr)
+    : "memory");
+  return prev;
+}
+#else
+static inline noit_atomic64_t
+noit_atomic_cas64(volatile noit_atomic64_t *ptr
+                  volatile noit_atomic64_t rpl,
+                  volatile noit_atomic64_t curr) {
+  noit_atomic64_t prev;
+  __asm__ volatile (
+      "pushl %%ebx;"
+      "mov 4+%1,%%ecx;"
+      "mov %1,%%ebx;"
+      "lock;"
+      "cmpxchg8b (%3);"
+      "popl %%ebx"
+    : "=A" (prev)
+    : "m" (rpl), "A" (curr), "r" (ptr)
+    : "%ebx", "%ecx", "memory");
+  return prev;
+};
+#endif
+
 #else
 #error Please stub out the atomics section for your platform
 #endif
