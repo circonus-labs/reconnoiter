2c65def60e6630d515d47a2135e89e7961c4c5b9
Theo Schlossnagle <jesus@omniti.com>
2008-01-26 04:20:28 +0000

    take some options

--- src/noitd.c
+++ src/noitd.c
@@ -2,23 +2,49 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <errno.h>
 #include <sys/ioctl.h>
 #include <fcntl.h>
 
+#include "getopt_long.h"
 #include "eventer/eventer.h"
 #include "utils/noit_log.h"
 #include "noit_listener.h"
 #include "noit_console.h"
+#include "noit_conf.h"
+
+static char *config_file = ETC_DIR "/noit.conf";
+static int debug = 0;
+
+void parse_clargs(int argc, char **argv) {
+  int c;
+  while((c = getopt(argc, argv, "c:d")) != EOF) {
+    switch(c) {
+      case 'c':
+        config_file = strdup(optarg);
+        break;
+      case 'd':
+        debug++;
+        break;
+      default:
+        break;
+    }
+  }
+}
 
 int main(int argc, char **argv) {
+  parse_clargs(argc, argv);
 
   noit_log_init();
-  noit_log_stream_add_stream(noit_debug, noit_stderr);
+  if(debug)
+    noit_log_stream_add_stream(noit_debug, noit_stderr);
   noit_log_stream_add_stream(noit_error, noit_stderr);
 
   noit_conf_init();
-
+  if(noit_conf_load(config_file) == -1) {
+    fprintf(stderr, "Cannot load config: '%s'\n", config_file);
+  }
   if(eventer_choose("kqueue") == -1) {
     fprintf(stderr, "Cannot choose kqueue\n");
     exit(-1);
