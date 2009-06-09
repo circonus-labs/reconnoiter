c30a0fee9505231eb5c6c83767095223ce9a1939
Theo Schlossnagle <jesus@omniti.com>
2009-06-09 14:55:10 +0000

    this is cheaper

--- src/noit_xml.c
+++ src/noit_xml.c
@@ -1,5 +1,37 @@
+/*
+ * Copyright (c) 2007, OmniTI Computer Consulting, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *     * Neither the name OmniTI Computer Consulting, Inc. nor the names
+ *       of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written
+ *       permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #include "noit_xml.h"
+#include <assert.h>
 
 struct noit_xml_buffer_ptr {
   char *buff;
@@ -39,19 +71,16 @@ noit_xml_save_closer(void *vstr) {
 
 char *
 noit_xmlSaveToBuffer(xmlDocPtr doc) {
-  char *outbuff;
   xmlOutputBufferPtr out;
   xmlCharEncodingHandlerPtr enc;
-  struct noit_xml_buffer_ptr *buf;
+  struct noit_xml_buffer_ptr buf = { NULL,0,0,0 };
 
-  buf = calloc(1, sizeof(*buf));
   enc = xmlGetCharEncodingHandler(XML_CHAR_ENCODING_UTF8);
   out = xmlOutputBufferCreateIO(noit_xml_save_writer,
                                 noit_xml_save_closer,
-                                buf, enc);
+                                &buf, enc);
+  assert(out);
   xmlSaveFormatFileTo(out, doc, "utf8", 1);
-  outbuff = buf->buff;
-  free(buf);
-  return outbuff;
+  return buf.buff;
 }
 
