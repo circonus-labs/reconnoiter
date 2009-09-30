77e875df87c94f8cd2b8fbc32fbe360623e759b8
Robert Treat <robert@omniti.com>
2009-09-30 16:14:10 +0000

    adding schema for rollup tables, with array based storage

--- sql/tables/noit.metric_numeric_rollup_20m.sql
+++ sql/tables/noit.metric_numeric_rollup_20m.sql
@@ -1,1 +1,63 @@
--- formerly rollup_matrix_numberic_20m
+-- formerly rollup_matrix_numeric_20m
+--
+-- PostgreSQL database dump
+--
+
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = off;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET escape_string_warning = off;
+
+SET search_path = noit, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: metric_numeric_rollup_20m; Type: TABLE; Schema: noit; Owner: reconnoiter; Tablespace: 
+--
+
+CREATE TABLE metric_numeric_rollup_20m (
+    sid integer NOT NULL,
+    name text NOT NULL,
+    rollup_time timestamp with time zone NOT NULL,
+    count_rows integer[],
+    avg_value numeric[],
+    counter_dev numeric[]
+);
+
+
+ALTER TABLE noit.metric_numeric_rollup_20m OWNER TO reconnoiter;
+
+--
+-- Name: metric_numeric_rollup_20m_new_pkey; Type: CONSTRAINT; Schema: noit; Owner: reconnoiter; Tablespace: 
+--
+
+ALTER TABLE ONLY metric_numeric_rollup_20m
+    ADD CONSTRAINT metric_numeric_rollup_20m_pkey PRIMARY KEY (rollup_time, sid, name);
+
+
+--
+-- Name: metric_numeric_rollup_20m_rollup_time_idx; Type: INDEX; Schema: noit; Owner: reconnoiter; Tablespace: 
+--
+
+CREATE INDEX metric_numeric_rollup_20m_rollup_time_idx ON metric_numeric_rollup_20m USING btree (date_hour(rollup_time));
+
+
+--
+-- Name: metric_numeric_rollup_20m; Type: ACL; Schema: noit; Owner: reconnoiter
+--
+
+REVOKE ALL ON TABLE metric_numeric_rollup_20m FROM PUBLIC;
+REVOKE ALL ON TABLE metric_numeric_rollup_20m FROM reconnoiter;
+GRANT ALL ON TABLE metric_numeric_rollup_20m TO reconnoiter;
+GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE metric_numeric_rollup_20m TO stratcon;
+GRANT SELECT ON TABLE metric_numeric_rollup_20m TO prism;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
--- sql/tables/noit.metric_numeric_rollup_60m.sql
+++ sql/tables/noit.metric_numeric_rollup_60m.sql
@@ -1,1 +1,56 @@
--- formerly rollup_matrix_numberic_60m 
+-- formerly rollup_matrix_numeric_60m 
+--
+-- PostgreSQL database dump
+--
+
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = off;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET escape_string_warning = off;
+
+SET search_path = noit, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: metric_numeric_rollup_60m; Type: TABLE; Schema: noit; Owner: reconnoiter; Tablespace: 
+--
+
+CREATE TABLE metric_numeric_rollup_60m (
+    sid integer NOT NULL,
+    name text NOT NULL,
+    rollup_time timestamp with time zone NOT NULL,
+    count_rows integer[],
+    avg_value numeric[],
+    counter_dev numeric[]
+);
+
+
+ALTER TABLE noit.metric_numeric_rollup_60m OWNER TO reconnoiter;
+
+--
+-- Name: metric_numeric_rollup_60m_pkey; Type: CONSTRAINT; Schema: noit; Owner: reconnoiter; Tablespace: 
+--
+
+ALTER TABLE ONLY metric_numeric_rollup_60m
+    ADD CONSTRAINT metric_numeric_rollup_60m_pkey PRIMARY KEY (rollup_time, sid, name);
+
+
+--
+-- Name: metric_numeric_rollup_60m; Type: ACL; Schema: noit; Owner: reconnoiter
+--
+
+REVOKE ALL ON TABLE metric_numeric_rollup_60m FROM PUBLIC;
+REVOKE ALL ON TABLE metric_numeric_rollup_60m FROM reconnoiter;
+GRANT ALL ON TABLE metric_numeric_rollup_60m TO reconnoiter;
+GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE metric_numeric_rollup_60m TO stratcon;
+GRANT SELECT ON TABLE metric_numeric_rollup_60m TO prism;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
