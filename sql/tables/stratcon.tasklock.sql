81f7001e39e1c3484d225b7a609fc803c86ba5ef
Robert Treat <robert@omniti.com>
2009-09-30 17:04:25 +0000

    new task locking infrastructure

--- sql/tables/stratcon.tasklock.sql
+++ sql/tables/stratcon.tasklock.sql
@@ -1,1 +1,42 @@
--- formerly tasklocks
+--
+-- PostgreSQL database dump
+--
+
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = off;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET escape_string_warning = off;
+
+SET search_path = stratcon, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: tasklock_id_seq; Type: SEQUENCE; Schema: stratcon; Owner: reconnoiter
+--
+
+CREATE SEQUENCE tasklock_id_seq
+    INCREMENT BY 1
+    NO MAXVALUE
+    NO MINVALUE
+    CACHE 1;
+
+ALTER TABLE stratcon.tasklock_id_seq OWNER TO reconnoiter;
+
+--
+-- Name: tasklock_id_seq; Type: ACL; Schema: stratcon; Owner: reconnoiter
+--
+
+REVOKE ALL ON SEQUENCE tasklock_id_seq FROM PUBLIC;
+REVOKE ALL ON SEQUENCE tasklock_id_seq FROM reconnoiter;
+GRANT ALL ON SEQUENCE tasklock_id_seq TO reconnoiter;
+GRANT SELECT,UPDATE ON SEQUENCE tasklock_id_seq TO stratcon;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
