7bdcc136d61994452c7859ab1437d1b6a3af6402
Denish Patel <denish@omniti.com>
2009-10-02 18:27:51 +0000

    updated noit.update_mns_via_check_tag

--- sql/sprocs/noit.update_mns_via_check_tag.sql
+++ sql/sprocs/noit.update_mns_via_check_tag.sql
@@ -1,1 +1,15 @@
 -- formerly stratcon.trig_update_tsvector_from_check_tags()
+
+CREATE OR REPLACE FUNCTION noit.update_mns_via_check_tag()
+  RETURNS trigger AS
+$BODY$
+DECLARE
+BEGIN
+    UPDATE noit.metric_name_summary SET ts_search_all=stratcon.metric_name_summary_tsvector(NEW.sid,metric_name,metric_type)
+    where sid=NEW.sid;
+   RETURN NEW;
+END
+$BODY$
+  LANGUAGE 'plpgsql'   SECURITY DEFINER;
+
+GRANT EXECUTE ON FUNCTION noit.update_mns_via_check_tag() TO stratcon;
