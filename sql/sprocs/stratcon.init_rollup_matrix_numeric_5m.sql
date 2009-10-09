c24c74b1cb20b7f00adc69391d316f16c8da4d31
Robert Treat <robert@omniti.com>
2009-10-09 19:06:57 +0000

    initialize emtpy arrays for storage

---
+++ sql/sprocs/stratcon.init_rollup_matrix_numeric_5m.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE FUNCTION stratcon.init_rollup_matrix_numeric_5m()
+RETURNS SETOF noit.metric_numeric_rollup_5m 
+AS $$
+  SELECT null::int, null::text, null::timestamptz, array_agg(count_rows), array_agg(avg_value), array_agg(counter_dev) from (select NULL::int as count_rows, NULL::numeric as avg_value, NULL::numeric as counter_dev from generate_series(1,288)) x;
+$$ 
+LANGUAGE 'sql'
+SECURITY DEFINER 
+; 
+
+
