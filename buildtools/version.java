c380e910ac189a14dc20d41cd412b4adc5a51b6c
Theo Schlossnagle <jesus@omniti.com>
2010-08-06 02:53:50 +0000

    make sure we're running Java6... it's needed

---
+++ buildtools/version.java
@@ -0,0 +1,14 @@
+import java.lang.System;
+public class version {
+  public static void main(String a[]) {
+    Double v = Double.valueOf(System.getProperty("java.specification.version"));
+    if(a.length > 0) {
+      Double want = Double.valueOf(a[0]);
+      if(want >= v) System.exit(0);
+    }
+    else {
+      System.out.println(v);
+    }
+    System.exit(-1);
+  }
+}
