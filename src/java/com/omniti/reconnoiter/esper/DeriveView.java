30edb2479a4f5ca9ff9ed942ecc48009549c8920
Theo Schlossnagle <jesus@omniti.com>
2010-10-14 11:38:33 +0000

    annoying line wrap -- no functional change

--- src/java/com/omniti/reconnoiter/esper/DeriveView.java
+++ src/java/com/omniti/reconnoiter/esper/DeriveView.java
@@ -1,12 +1,13 @@
-/**************************************************************************************
- * Copyright (C) 2008 EsperTech, Inc. All rights reserved.                            *
- * Copyright (C) 2009 OmniTI, Inc. All rights reserved.                               *
- * http://esper.codehaus.org                                                          *
- * http://www.espertech.com                                                           *
- * ---------------------------------------------------------------------------------- *
- * The software in this package is published under the terms of the GPL license       *
- * a copy of which has been included with this distribution in the license.txt file.  *
- **************************************************************************************/
+/****************************************************************************
+ * Copyright (C) 2008 EsperTech, Inc. All rights reserved.                  *
+ * Copyright (C) 2009 OmniTI, Inc. All rights reserved.                     *
+ * http://esper.codehaus.org                                                *
+ * http://www.espertech.com                                                 *
+ * ------------------------------------------------------------------------ *
+ * The software in this package is published under the terms of the         *
+ * GPL license a copy of which has been included with this distribution     *
+ * in the license.txt file.                                                 *
+ ****************************************************************************/
 
 package com.omniti.reconnoiter.esper;
 
