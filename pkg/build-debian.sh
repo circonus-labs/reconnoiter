1c72b25e1ee042e2a8e8a16eec7239c98e6cd892
Brandon Philips <brandon.philips@rackspace.com>
2011-06-09 15:39:49 -0700

    pkg: fix up debian package to actually work
    A number of changes have left the debian package broken including the
    javalibdir changes and version bumps. Fix these minor issues.
    
    Then I also automated the version number creation so that every time you
    call pkg/build-debian.sh it generates a new version number if the HEAD
    has moved. It is a little hacky but solves the version bumping issue for
    now.

--- pkg/build-debian.sh
+++ pkg/build-debian.sh
@@ -4,6 +4,15 @@ set -e
 
 ln -sf pkg/debian debian
 
+HEADFILE="pkg/HEAD"
+HEAD=`git log -1 HEAD --pretty=%h`
+[ -f $HEADFILE ] && OLDHEAD=`cat $HEADFILE`
+
+if [ "x$HEAD" != "x$OLDHEAD" ]; then
+	dch -v `date +"%Y%m%d%H%M%S"` "HEAD `git log -1 HEAD --abbrev-commit --pretty=oneline`"
+	echo $HEAD > $HEADFILE
+fi
+
 dpkg-buildpackage -rfakeroot -tc
 
 rm debian
