b184cd746dbfe5e4f27c4ed27acab1f9c4898e2e
Ryan Phillips <ryan.phillips@rackspace.com>
2011-11-09 16:01:06 -0600

    add libcidr dep

--- src/noit_acl.c
+++ src/noit_acl.c
@@ -6,65 +6,66 @@
 #include "noit_check.h"
 #include "noit_conf_checks.h"
 #include "noit_acl.h"
+#include "libcidr.h"
 
 #include <pcre.h>
 #include <assert.h>
 #include <libxml/tree.h>
 
-typedef struct _aclnetwork {
-  char *ip_range;
-  struct _aclnetwork *next;
-} aclnetwork_t;
+typedef enum {
+  ACL_ALLOW = 0,
+  ACL_DENY
+} acltype_t;
+
+typedef struct _aclcidr_t {
+  CIDR *cidr;
+  struct _aclcidr_t *next;
+} aclcidr_t;
 
 typedef struct {
   noit_atomic32_t ref_cnt;
   char *name;
-  char *type;
-  aclnetwork_t *networks;
-  noit_hash_table *ip;
+  acltype_t type;
+  aclcidr_t *cidrs;
 } aclset_t;
 
 static noit_hash_table *aclsets = NULL;
 
 static void
-noit_aclnetwork_free(void *vp) {
-  aclnetwork_t *n = vp;
-  free(n->ip_range);
+noit_aclcidr_free(void *vp) {
+  aclcidr_t *n = vp;
+  cidr_free(n->cidr);
   free(n);
 }
 
-static aclnetwork_t*
-noit_aclnetwork_create(const char *ip_range) {
-  aclnetwork_t *net = calloc(1, sizeof(*net));
-  net->ip_range = strdup(ip_range);
-  return net;
+static aclcidr_t*
+noit_aclcidr_create(const char *range) {
+  aclcidr_t *c = calloc(1, sizeof(*c));
+  c->cidr = cidr_from_str(range);
+  return c;
 }
 
 static void
 aclset_free(void *vp) {
   aclset_t *as = vp;
-  aclnetwork_t *n;
+  aclcidr_t *n;
   if(noit_atomic_dec32(&as->ref_cnt) != 0) return;
   noitL(noit_error, "Freeing acl [%d]: %s\n", as->ref_cnt, as->name);
-  while(as->networks) {
-    n = as->networks->next;
-    noit_aclnetwork_free(as->networks);
-    as->networks = n;
+  while(as->cidrs) {
+    n = as->cidrs->next;
+    noit_aclcidr_free(as->cidrs);
+    as->cidrs = n;
   }
-  noit_hash_destroy(as->ip, NULL, NULL);
   if(as->name) free(as->name);
-  if(as->type) free(as->type);
   free(as);
 }
 
 static aclset_t*
-noit_aclset_create(const char *name, const char *type) {
+noit_acl_create(const char *name, const char *type) {
   aclset_t *set = calloc(1, sizeof(*set));
   set->ref_cnt = 1;
   set->name = strdup(name);
-  set->type = strdup(type);
-  set->ip = calloc(1, sizeof(noit_hash_table*));
-  noit_hash_init(set->ip);
+  set->type = strcasecmp(type, "deny") == 0 ? ACL_DENY : ACL_ALLOW;
   return set;
 }
 
@@ -80,13 +81,10 @@ noit_acl_from_conf() {
   free(sets);
 }
 void
-noit_acl_add_network(aclset_t *set, aclnetwork_t *net) {
-  net->next = set->networks;
-  set->networks = net;
-}
-void
-noit_acl_add_ip(aclset_t *set, const char *ip) {
-  noit_hash_store(set->ip, ip, strlen(ip), NULL);
+noit_acl_add_cidr(aclset_t *set, const char *range) {
+  aclcidr_t *cidr = noit_aclcidr_create(range);
+  cidr->next = set->cidrs;
+  set->cidrs = cidr;
 }
 void
 noit_acl_init() {
@@ -113,12 +111,14 @@ noit_acl_add(noit_conf_section_t setinfo) {
 
   if(!noit_conf_get_stringbuf(setinfo, "@type",
                               acl_type, sizeof(acl_type))) {
-    strcpy(acl_type, "deny");
+    noitL(noit_error,
+          "acl with no type, skipping as it cannot be referenced.\n");
+    return;
   }
 
   noitL(noit_debug, "loaded ACL (name=%s, type=%s)\n", acl_name, acl_type);
 
-  set = noit_aclset_create(acl_name, acl_type);
+  set = noit_acl_create(acl_name, acl_type);
   networks = noit_conf_get_sections(setinfo, "network", &fcnt);
 
   for(j=fcnt-1; j>=0; j--) {
@@ -128,14 +128,7 @@ noit_acl_add(noit_conf_section_t setinfo) {
       continue;
     }
     noitL(noit_debug, "  %s\n", buffer);
-    if (strstr(buffer, "/")) {
-      // network
-      noit_acl_add_network(set, noit_aclnetwork_create(buffer));
-    }
-    else {
-      // ip
-      noit_acl_add_ip(set, buffer);
-    }
+    noit_acl_add_cidr(set, buffer);
   }
 
   noit_hash_replace(aclsets, set->name, strlen(set->name), (void *)set,
@@ -143,5 +136,6 @@ noit_acl_add(noit_conf_section_t setinfo) {
 }
 noit_boolean
 noit_acl_check_ip(const char *ip) {
+  // check networks
   return noit_false;
 }
