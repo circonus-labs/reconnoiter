7ee5734c5784243f51f59c170d36bf2be49b1caa
Ryan Phillips <ryan.phillips@rackspace.com>
2011-12-13 11:25:21 -0600

    fix link list

--- src/utils/noit_hooks.c
+++ src/utils/noit_hooks.c
@@ -42,8 +42,8 @@ noit_hooks_add(noit_hooks_t *nh, const char *name,
   noit_hook_t *hook = NULL;;
   noit_hook_t *new_hook = calloc(1, sizeof(*new_hook));
   new_hook->func = func;
-  new_hook->next = hook;
   if (noit_hash_retrieve(&nh->hooks, name, strlen(name), (void**)&hook)) {
+    new_hook->next = hook;
     noit_hash_replace(&nh->hooks, strdup(name), strlen(name), (void *)new_hook, NULL, NULL);
   }
   else {
