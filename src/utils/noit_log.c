3370fd8396d2d2dbedae6732f511f0446827e274
Theo Schlossnagle <jesus@omniti.com>
2012-09-14 13:18:09 -0400

    indicate what the failure was

--- src/utils/noit_log.c
+++ src/utils/noit_log.c
@@ -389,7 +389,7 @@ jlog_logio_asynch_writer(void *vls) {
                                      line->buf_dynamic :
                                      line->buf_static,
                         line->len) == -1) {
-        noitL(noit_error, "jlog_ctx_write failed: fatal\n");
+        noitL(noit_error, "jlog_ctx_write failed: %s\n", jlog_ctx_err_string(actx->log));
         abort();
       }
       if(line->buf_dynamic != NULL) free(line->buf_dynamic);
