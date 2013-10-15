ae724278f4f19de22e89bcd421ede662d4c6c699
Theo Schlossnagle <jesus@omniti.com>
2013-10-15 03:46:37 +0000

    fix warnings on Illumos

--- src/modules/lua_noit.c
+++ src/modules/lua_noit.c
@@ -2211,7 +2211,7 @@ nl_check(lua_State *L) {
   noit_check_t *check;
   uuid_t id;
   const char *id_str = lua_tostring(L,1);
-  if(id_str && uuid_parse(id_str, id) == 0) {
+  if(id_str && uuid_parse((char *)id_str, id) == 0) {
     noit_check_t *check = noit_poller_lookup(id);
     if(check) {
       noit_lua_setup_check(L, check);
