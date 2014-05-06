f7e50c7f60e5886f20ad74ba3eba9df87ae346c7
Theo Schlossnagle <jesus@omniti.com>
2014-05-06 11:42:25 -0400

    Fix typo in typedef

--- src/noit_defines.h
+++ src/noit_defines.h
@@ -5,7 +5,7 @@
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
  * met:
- * 
+ *
  *     * Redistributions of source code must retain the above copyright
  *       notice, this list of conditions and the following disclaimer.
  *     * Redistributions in binary form must reproduce the above
@@ -16,7 +16,7 @@
  *       of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written
  *       permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -135,7 +135,7 @@ typedef uint64_t noit_hrtime_t;
 #elif defined(linux) || defined(__linux) || defined(__linux__)
 typedef long long unsigned int noit_hrtime_t;
 #else
-typedef htrime_t noit_hrtime_t;
+typedef hrtime_t noit_hrtime_t;
 #endif
 
 #if defined(linux)
