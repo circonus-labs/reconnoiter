376300203ae56684650f59bd0e6aae8c8f00065f
Theo Schlossnagle <jesus@omniti.com>
2015-02-18 14:04:23 -0500

    cleanup dtrace header deps

--- buildtools/culldeps.sh
+++ buildtools/culldeps.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-perl -ni -e 's/^(\S+)\.o:/$1.o $1.lo:/g; s/(\s)\/\S+/$1/g; s/(\s)\S+dtrace_probes\.h/$1/g; next if /^\s*\\?$/; print "\n" if /^\S/; s/^\s+(?=\S)/  /g; print;' $*
+perl -ni -e 's/^(\S+)\.o:/$1.o $1.lo:/g; s/(\s)\/\S+/$1/g; next if /^\s*\\?$/; print "\n" if /^\S/; s/^\s+(?=\S)/  /g; print;' $*
