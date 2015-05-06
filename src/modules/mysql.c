7b25fe3fe82c2f8e75d36eb6df7c9f19289ccb4c
Theo Schlossnagle <jesus@omniti.com>
2015-05-06 10:44:00 -0400

    fix mysql module to use new stasts API

--- src/modules/mysql.c
+++ src/modules/mysql.c
@@ -117,7 +117,7 @@ static void mysql_ingest_stats(mysql_check_info_t *ci) {
               iv = strtol(row[j], NULL, 10);
               piv = &iv;
             }
-            noit_stats_set_metric(ci->check, &ci->check->stats.inprogress, mname, METRIC_INT32, piv);
+            noit_stats_set_metric(ci->check, mname, METRIC_INT32, piv);
             break;
           case FIELD_TYPE_INT24:
           case FIELD_TYPE_LONGLONG:
@@ -126,7 +126,7 @@ static void mysql_ingest_stats(mysql_check_info_t *ci) {
               lv = strtoll(row[j], NULL, 10);
               plv = &lv;
             }
-            noit_stats_set_metric(ci->check, &ci->check->stats.inprogress, mname, METRIC_INT64, plv);
+            noit_stats_set_metric(ci->check, mname, METRIC_INT64, plv);
             break;
           case FIELD_TYPE_DECIMAL:
           case FIELD_TYPE_FLOAT:
@@ -136,12 +136,12 @@ static void mysql_ingest_stats(mysql_check_info_t *ci) {
               dv = atof(row[j]);
               pdv = &dv;
             }
-            noit_stats_set_metric(ci->check, &ci->check->stats.inprogress, mname, METRIC_DOUBLE, pdv);
+            noit_stats_set_metric(ci->check, mname, METRIC_DOUBLE, pdv);
             break;
           default:
             if(!row[j]) sv = NULL;
             else sv = row[j];
-            noit_stats_set_metric(ci->check, &ci->check->stats.inprogress, mname, METRIC_GUESS, sv);
+            noit_stats_set_metric(ci->check, mname, METRIC_GUESS, sv);
             break;
         }
       }
@@ -149,38 +149,38 @@ static void mysql_ingest_stats(mysql_check_info_t *ci) {
   }
 }
 static void mysql_log_results(noit_module_t *self, noit_check_t *check) {
-  struct timeval duration;
+  struct timeval duration, now;
   mysql_check_info_t *ci = check->closure;
 
-  gettimeofday(&ci->check->stats.inprogress.whence, NULL);
-  sub_timeval(ci->check->stats.inprogress.whence, check->last_fire_time, &duration);
-  ci->check->stats.inprogress.duration = duration.tv_sec * 1000 + duration.tv_usec / 1000;
-  ci->check->stats.inprogress.available = NP_UNAVAILABLE;
-  ci->check->stats.inprogress.state = NP_BAD;
-  if(ci->error) ci->check->stats.inprogress.status = ci->error;
-  else if(ci->timed_out) ci->check->stats.inprogress.status = "timeout";
+  gettimeofday(&now, NULL);
+  sub_timeval(now, check->last_fire_time, &duration);
+  noit_stats_set_whence(check, &now);
+  noit_stats_set_duration(check, duration.tv_sec * 1000 + duration.tv_usec / 1000);
+  noit_stats_set_available(check, NP_UNAVAILABLE);
+  noit_stats_set_state(check, NP_BAD);
+  if(ci->error) noit_stats_set_status(check, ci->error);
+  else if(ci->timed_out) noit_stats_set_status(check, "timeout");
   else if(ci->rv == 0) {
-    ci->check->stats.inprogress.available = NP_AVAILABLE;
-    ci->check->stats.inprogress.state = NP_GOOD;
-    ci->check->stats.inprogress.status = "no rows, ok";
+    noit_stats_set_available(check, NP_AVAILABLE);
+    noit_stats_set_state(check, NP_GOOD);
+    noit_stats_set_status(check, "no rows, ok");
   }
   else {
-    ci->check->stats.inprogress.available = NP_AVAILABLE;
-    ci->check->stats.inprogress.state = NP_GOOD;
-    ci->check->stats.inprogress.status = "got rows, ok";
+    noit_stats_set_available(check, NP_AVAILABLE);
+    noit_stats_set_state(check, NP_GOOD);
+    noit_stats_set_status(check, "got rows, ok");
   }
 
   if(ci->rv >= 0)
-    noit_stats_set_metric(check, &check->stats.inprogress, "row_count", METRIC_INT32, &ci->rv);
+    noit_stats_set_metric(check, "row_count", METRIC_INT32, &ci->rv);
   if(ci->connect_duration)
-    noit_stats_set_metric(check, &check->stats.inprogress, "connect_duration", METRIC_DOUBLE,
+    noit_stats_set_metric(check, "connect_duration", METRIC_DOUBLE,
                           ci->connect_duration);
   if(ci->query_duration)
-    noit_stats_set_metric(check, &check->stats.inprogress, "query_duration", METRIC_DOUBLE,
+    noit_stats_set_metric(check, "query_duration", METRIC_DOUBLE,
                           ci->query_duration);
 
-  noit_check_set_stats(check, &ci->check->stats.inprogress);
-  noit_check_stats_clear(check, &check->stats.inprogress);
+  noit_check_set_stats(check);
 }
 
 #define FETCH_CONFIG_OR(key, str) do { \
@@ -267,7 +267,6 @@ static int mysql_drive_session(eventer_t e, int mask, void *closure,
   }
   switch(mask) {
     case EVENTER_ASYNCH_WORK:
-      noit_check_stats_clear(ci->check, &ci->check->stats.inprogress);
       ci->connect_duration = NULL;
       ci->query_duration = NULL;
 
