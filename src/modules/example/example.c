40656bf5be22c99a2de77a8411c6db36114600c5
Theo Schlossnagle <jesus@omniti.com>
2015-05-06 11:01:32 -0400

    fix example module to use new stats API

--- src/modules/example/example.c
+++ src/modules/example/example.c
@@ -1,21 +1,21 @@
 /* Your copyright and license data here */
 
-#include "noit_defines.h"
+#include "mtev_defines.h"
 #include "noit_module.h"
 #include "noit_check.h"
 #include "noit_check_tools.h"
 
-static noit_log_stream_t nlerr = NULL;
-static noit_log_stream_t nldeb = NULL;
+static mtev_log_stream_t nlerr = NULL;
+static mtev_log_stream_t nldeb = NULL;
 
 
-static int example_onload(noit_image_t *self) {
-  if(NULL == (nlerr=noit_log_stream_find("error/example"))) nlerr = noit_error;
-  if(NULL == (nldeb=noit_log_stream_find("debug/example"))) nldeb = noit_debug;
+static int example_onload(mtev_image_t *self) {
+  if(NULL == (nlerr=mtev_log_stream_find("error/example"))) nlerr = mtev_error;
+  if(NULL == (nldeb=mtev_log_stream_find("debug/example"))) nldeb = mtev_debug;
   return 0;
 }
 
-static int example_config(noit_module_t *self, noit_hash_table *options) {
+static int example_config(noit_module_t *self, mtev_hash_table *options) {
   return 0;
 }
 
@@ -33,35 +33,32 @@ static int example_initiate(noit_module_t *self, noit_check_t *check,
                             noit_check_t *cause) {
   struct example_check_info *ci = check->closure;
   const char *limit = "0";
-  struct timeval diff;
+  struct timeval now, diff;
 
   BAIL_ON_RUNNING_CHECK(check);
   check->flags |= NP_RUNNING;
 
-  noit_hash_retrieve(check->config, "limit", strlen("limit"), (void **)&limit);
+  mtev_hash_retrieve(check->config, "limit", strlen("limit"), (void **)&limit);
   ci->limit = atoi(limit);
 
-  if(check->stats.inprogress.status) free(check->stats.inprogress.status);
-  noit_check_stats_clear(check, &check->stats.inprogress); 
-  gettimeofday(&check->stats.inprogress.whence, NULL);
-  sub_timeval(check->stats.inprogress.whence, check->last_fire_time, &diff);
-  check->stats.inprogress.duration = diff.tv_sec * 1000 + diff.tv_usec / 1000;
-  check->stats.inprogress.available = NP_AVAILABLE;
-  check->stats.inprogress.status = strdup("hello world");
+  gettimeofday(&now, NULL);
+  sub_timeval(now, check->last_fire_time, &diff);
+  noit_stats_set_whence(check, &now);
+  noit_stats_set_duration(check, diff.tv_sec * 1000 + diff.tv_usec / 1000);
+  noit_stats_set_available(check, NP_AVAILABLE);
+  noit_stats_set_status(check, "hello world");
 
   if(ci->limit) {
     int value = (int)(lrand48() % ci->limit);
-    noit_stats_set_metric(check, &check->stats.inprogress,
-                          "random", METRIC_INT32, &value);
-    check->stats.inprogress.state = NP_GOOD;
+    noit_stats_set_metric(check, "random", METRIC_INT32, &value);
+    noit_stats_set_state(check, NP_GOOD);
   }
   else {
-    noit_stats_set_metric(check, &check->stats.inprogress,
-                          "random", METRIC_INT32, NULL);
-    check->stats.inprogress.state = NP_BAD;
+    noit_stats_set_metric(check, "random", METRIC_INT32, NULL);
+    noit_stats_set_state(check, NP_BAD);
   }
 
-  noit_check_set_stats(check, &check->stats.inprogress);
+  noit_check_set_stats(check);
   check->flags &= ~NP_RUNNING;
 
   return 0;
