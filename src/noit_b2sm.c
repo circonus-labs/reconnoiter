6e895e2b46074b5e1d1e81b2a0e0621486ead738
Theo Schlossnagle <theo.schlossnagle@circonus.com>
2017-02-11 03:02:58 +0000

    make noit_b2sm tolerate spaces and guess at noit presence

--- src/noit_b2sm.c
+++ src/noit_b2sm.c
@@ -4,30 +4,31 @@
 #include "noit_check_log_helpers.h"
 
 static void usage(const char *prog) {
-  fprintf(stderr, "%s [-stratcon]\n", prog);
-  fprintf(stderr, "\t-stratcon] assumes the second field is an IP\n\n");
+  fprintf(stderr, "%s\n", prog);
   fprintf(stderr, "This tool takes B{1,2} records from stdin and emits S/M records to stdout\n");
 }
 char buff[1024*1024*16];
 int main(int argc, char **argv) {
-  int expect_ip = 0;
-  if(argc > 2 || (argc == 2 && strcmp(argv[1], "-stratcon"))) {
+  if(argc > 1) {
     usage(argv[0]);
     return 2;
   }
-  if(argc == 2) expect_ip = 1;
   while(fgets(buff, sizeof(buff), stdin) != NULL) {
     char **lines = NULL;
     int i;
-    char *ip = buff, *cp = buff;
-    while(*cp == ' ') cp++;
-    while(*cp) {
-      if(*cp == ' ' && ip[-1] != '\t') *ip++ = '\t';
-      else if(*cp != ' ') *ip++ = *cp;
-      cp++;
+    char *dp = buff, *sp = buff;
+    while(*sp) {
+      if(*sp == ' ' || *sp == '\t') {
+        if(dp > buff && dp[-1] == '\t') {
+          sp++; continue;
+        }
+        *dp = '\t';
+      }
+      else *dp = *sp;
+      sp++; dp++;
     }
-    *ip = '\0';
-    int nm = noit_check_log_b_to_sm(buff, strlen(buff), &lines, expect_ip);
+    *dp = '\0';
+    int nm = noit_check_log_b_to_sm(buff, strlen(buff), &lines, -1);
     for(i=0; i<nm; i++) {
       /* zip through the line and change spaces to tabs. */
       printf("%s\n", lines[i]);
