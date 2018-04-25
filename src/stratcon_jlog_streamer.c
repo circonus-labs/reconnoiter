2fb985d3326e284ee3a5699ca025b0f292a80d67
Phil Maddox <philip.maddox@circonus.com>
2018-04-25 17:30:00 +0000

    Formatting Change To Debug Print

--- src/stratcon_jlog_streamer.c
+++ src/stratcon_jlog_streamer.c
@@ -95,7 +95,7 @@ static const char *jlog_state_to_str(int state) {
 }
 
 static void change_state(jlog_streamer_ctx_t *ctx, mtev_connection_ctx_t *nctx, int new_state) {
-  mtevL(jlog_streamer_deb, "changing state from %s to %s - [%s] [%s]\n",
+  mtevL(jlog_streamer_deb, "changing state from \"%s\" to \"%s\" - [%s] [%s]\n",
           jlog_state_to_str(ctx->state),
           jlog_state_to_str(new_state),
           (nctx && nctx->remote_str) ? nctx->remote_str : "(null)",
