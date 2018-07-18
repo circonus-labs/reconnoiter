08486b70c001f3a0c70ad0a5e6a4012d93a72691
Riley Berton <riley@mosey.org>
2018-07-18 09:25:03 -0400

    fix typo

--- src/noit_message_decoder.c
+++ src/noit_message_decoder.c
@@ -421,7 +421,7 @@ noit_metric_tags_parse_one(const char *tagnm, size_t tagnmlen,
   /* make sure we covered everything */
   if(colon_pos == 0 || colon_pos >= cur_size - 1) return 0;
   /* tag category and name must combined be <= NOIT_TAG_MAX_PAIR_LEN */
-  if(cur_size > NOIT_TAG_MAX_PAIR_LEN)) return 0;
+  if(cur_size > NOIT_TAG_MAX_PAIR_LEN) return 0;
   output->total_size = cur_size;
   output->category_size = colon_pos + 1;
   output->tag = tagnm;
