ef5b65ba8b425a19ba09d120b086280a91852f8f
Theo Schlossnagle <theo.schlossnagle@circonus.com>
2018-08-15 15:17:40 +0000

    ignore text nodes within the XML

--- src/noit_check_rest.c
+++ src/noit_check_rest.c
@@ -649,6 +649,7 @@ noit_validate_check_rest_post(xmlDocPtr doc, xmlNodePtr *a, xmlNodePtr *c,
     if(!strcmp((char *)tl->name, "attributes")) {
       *a = tl;
       for(an = tl->children; an; an = an->next) {
+        if(an->type != XML_ELEMENT_NODE) continue;
 #define CHECK_N_SET(a) if(!strcmp((char *)an->name, #a))
         CHECK_N_SET(name) {
           xmlChar *tmp;
