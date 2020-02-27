41e824fbacc6cbca62dfd1b0a24ae1ff72b07f7c
Philip Maddox <philip.maddox@circonus.com>
2020-02-27 19:13:08 +0000

    Basic Framework For LMDB Stuff

---
+++ src/noit_lmdb_tools.c
@@ -0,0 +1,100 @@
+/* Copyright (c) 2020, Circonus, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *     * Neither the name OmniTI Computer Consulting, Inc. nor the names
+ *       of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written
+ *       permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOTK
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "noit_lmdb_tools.h"
+
+#include <stdlib.h>
+#include <errno.h>
+
+noit_lmdb_instance_t *noit_lmdb_tools_open_instance(char *path)
+{
+  int rc;
+  MDB_env *env;
+
+  rc = mdb_env_create(&env);
+  if (rc != 0) {
+    errno = rc;
+    return NULL;
+  }
+
+  /* let lots of threads read us */
+  rc = mdb_env_set_maxreaders(env, 1024);
+  if (rc != 0) {
+    errno = rc;
+    mdb_env_close(env);
+    return NULL;
+  }
+
+  rc = mdb_env_open(env, path, MDB_NOMETASYNC | MDB_NOSYNC | MDB_NOMEMINIT, 0644);
+  if (rc != 0) {
+    errno = rc;
+    mdb_env_close(env);
+    return NULL;
+  }
+
+  MDB_txn *txn;
+  MDB_dbi dbi;
+  rc = mdb_txn_begin(env, NULL, 0, &txn);
+  if (rc != 0) {
+    errno = rc;
+    mdb_env_close(env);
+    return NULL;
+  }
+  rc = mdb_dbi_open(txn, NULL, MDB_CREATE, &dbi);
+  if (rc != 0) {
+    mdb_txn_abort(txn);
+    mdb_env_close(env);
+    errno = rc;
+    return NULL;
+  }
+  rc = mdb_txn_commit(txn);
+  if (rc != 0) {
+    mdb_txn_abort(txn);
+    mdb_env_close(env);
+    errno = rc;
+    return NULL;
+  }
+
+  noit_lmdb_instance_t *instance = (noit_lmdb_instance_t *)malloc(sizeof(noit_lmdb_instance_t));
+  instance->env = env;
+  instance->dbi = dbi;
+  instance->path = strdup(path);
+
+  return instance;
+}
+
+void noit_lmdb_tools_close_instance(noit_lmdb_instance_t *instance)
+{
+  if (!instance) return;
+  mdb_dbi_close(instance->env, instance->dbi);
+  mdb_env_close(instance->env);
+  free(instance->path);
+  free(instance);
+}
