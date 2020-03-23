5da22648c6e4438990679706ece854a18dc49130
Philip Maddox <philip.maddox@circonus.com>
2020-03-23 20:51:37 +0000

    Placeholder Stuff For Delete

--- src/noit_check_lmdb.c
+++ src/noit_check_lmdb.c
@@ -630,12 +630,12 @@ noit_check_lmdb_set_check(mtev_http_rest_closure_t *restc,
   return 0;
 }
 
-
 int
 noit_check_lmdb_delete_check(mtev_http_rest_closure_t *restc,
                              int npats, char **pats) {
   mtev_http_session_ctx *ctx = restc->http_ctx;
   uuid_t checkid;
+  noit_check_t *check = NULL;
   int rc, error_code = 500;
   MDB_val mdb_key, mdb_data;
   MDB_txn *txn;
@@ -649,67 +649,86 @@ noit_check_lmdb_delete_check(mtev_http_rest_closure_t *restc,
   if(npats != 2) goto error;
   if(mtev_uuid_parse(pats[1], checkid)) goto error;
 
-  key = noit_lmdb_make_check_key_for_iterating(checkid, &key_size);
-  mtevAssert(key);
+  check = noit_poller_lookup(checkid);
 
-  mdb_key.mv_data = key;
-  mdb_key.mv_size = key_size;
-
-  ck_rwlock_read_lock(&instance->lock);
-  locked = true;
-
-  rc = mdb_txn_begin(instance->env, NULL, 0, &txn);
-  if (rc != 0) {
-    mtevL(mtev_error, "failed to create transaction for delete: %d (%s)\n", rc, mdb_strerror(rc));
-    goto error;
-  }
-  mdb_cursor_open(txn, instance->dbi, &cursor);
-  rc = mdb_cursor_get(cursor, &mdb_key, &mdb_data, MDB_SET_RANGE);
-  if (rc != 0) {
-    if (rc == MDB_NOTFOUND) {
-      mdb_cursor_close(cursor);
-      mdb_txn_abort(txn);
-      goto not_found;
+  /* delete this here */
+  mtev_boolean just_mark = mtev_false;
+  if(check) {
+    if(!noit_poller_deschedule(check->checkid, mtev_true, mtev_false)) {
+      just_mark = mtev_true;
     }
-    else {
-      mtevL(mtev_error, "failed on delete lookup: %d (%s)\n", rc, mdb_strerror(rc));
-      mdb_cursor_close(cursor);
-      mdb_txn_abort(txn);
+  }
+  if(just_mark) {
+    /* TODO: Figure out what to do here */
+    /*int64_t newseq = noit_conf_check_bump_seq(node);
+    xmlSetProp(node, (xmlChar *)"deleted", (xmlChar *)"deleted");
+    if(check) {
+      check->config_seq = newseq;
+      noit_cluster_mark_check_changed(check, NULL);
+    }
+    CONF_DIRTY(mtev_conf_section_from_xmlnodeptr(node)); */
+  }
+  else {
+    key = noit_lmdb_make_check_key_for_iterating(checkid, &key_size);
+    mtevAssert(key);
+
+    mdb_key.mv_data = key;
+    mdb_key.mv_size = key_size;
+
+    ck_rwlock_read_lock(&instance->lock);
+    locked = true;
+
+    rc = mdb_txn_begin(instance->env, NULL, 0, &txn);
+    if (rc != 0) {
+      mtevL(mtev_error, "failed to create transaction for delete: %d (%s)\n", rc, mdb_strerror(rc));
       goto error;
     }
-  }
-  while(rc == 0) {
-    noit_lmdb_check_data_t *data = noit_lmdb_check_data_from_key(mdb_key.mv_data);
-    if (data) {
-      if (memcmp(data->id, checkid, UUID_SIZE) != 0) {
-        noit_lmdb_free_check_data(data);
-        break;
+    mdb_cursor_open(txn, instance->dbi, &cursor);
+    rc = mdb_cursor_get(cursor, &mdb_key, &mdb_data, MDB_SET_RANGE);
+    if (rc != 0) {
+      if (rc == MDB_NOTFOUND) {
+        mdb_cursor_close(cursor);
+        mdb_txn_abort(txn);
+        goto not_found;
       }
-      mtevL(mtev_error, "DELETING KEY - TYPE %c, NAMESPACE %s, KEY %s\n", data->type, data->ns, data->key);
-      noit_lmdb_free_check_data(data);
-      rc = mdb_cursor_del(cursor, 0);
-      if (rc != 0) {
-        mtevL(mtev_error, "failed to delete key in check: %d (%s)\n", rc, mdb_strerror(rc));
+      else {
+        mtevL(mtev_error, "failed on delete lookup: %d (%s)\n", rc, mdb_strerror(rc));
         mdb_cursor_close(cursor);
         mdb_txn_abort(txn);
         goto error;
       }
     }
-    else {
-      break;
+    while(rc == 0) {
+      noit_lmdb_check_data_t *data = noit_lmdb_check_data_from_key(mdb_key.mv_data);
+      if (data) {
+        if (memcmp(data->id, checkid, UUID_SIZE) != 0) {
+          noit_lmdb_free_check_data(data);
+          break;
+        }
+        mtevL(mtev_error, "DELETING KEY - TYPE %c, NAMESPACE %s, KEY %s\n", data->type, data->ns, data->key);
+        noit_lmdb_free_check_data(data);
+        rc = mdb_cursor_del(cursor, 0);
+        if (rc != 0) {
+          mtevL(mtev_error, "failed to delete key in check: %d (%s)\n", rc, mdb_strerror(rc));
+          mdb_cursor_close(cursor);
+          mdb_txn_abort(txn);
+          goto error;
+        }
+      }
+      else {
+        break;
+      }
+      rc = mdb_cursor_get(cursor, &mdb_key, &mdb_data, MDB_NEXT);
     }
-    rc = mdb_cursor_get(cursor, &mdb_key, &mdb_data, MDB_NEXT);
-  }
-  rc = mdb_txn_commit(txn);
-  if (rc != 0) {
-    mtevL(mtev_error, "failed to commit delete txn: %d (%s)\n", rc, mdb_strerror(rc));
+    rc = mdb_txn_commit(txn);
+    if (rc != 0) {
+      mtevL(mtev_error, "failed to commit delete txn: %d (%s)\n", rc, mdb_strerror(rc));
+      mdb_cursor_close(cursor);
+      mdb_txn_abort(txn);
+      goto error;
+    }
     mdb_cursor_close(cursor);
-    mdb_txn_abort(txn);
-    goto error;
   }
-  mdb_cursor_close(cursor);
-
-  /* TODO: Remmove from running */
 
   mtev_http_response_ok(ctx, "text/html");
   mtev_http_response_end(ctx);
