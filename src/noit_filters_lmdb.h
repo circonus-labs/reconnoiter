fe46d3bdc7a3592411550df66d69191608d7f311
Philip Maddox <philip.maddox@circonus.com>
2020-05-12 16:17:49 +0000

    Remove Unused Enum

--- src/noit_filters_lmdb.h
+++ src/noit_filters_lmdb.h
@@ -33,13 +33,6 @@
 #include <mtev_rest.h>
 #include <libxml/tree.h>
 
-typedef enum {
-  NOIT_LMDB_FILTERSET_RULE_ACCEPT_TYPE = 'A',
-  NOIT_LMDB_FILTERSET_RULE_ALLOW_TYPE = 'L',
-  NOIT_LMDB_FILTERSET_RULE_DENY_TYPE = 'D',
-  NOIT_LMDB_FILTERSET_RULE_SKIPTO_TYPE = 'S',
-} noit_lmdb_filterset_rule_type_e;
-
 int noit_filters_lmdb_populate_filterset_xml_from_lmdb(xmlNodePtr root, char *fs_name);
 mtev_boolean noit_filters_lmdb_already_in_db(char *name);
 int64_t noit_filters_lmdb_get_seq(char *name);
