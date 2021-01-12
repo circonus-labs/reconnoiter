dbe7eaf8833324adda01b1c9e9899c6ad39f9d33
Theo Schlossnagle <theo.schlossnagle@circonus.com>
2021-01-12 10:40:26 -0500

    Up the default packets-per-cycle to 1000 in statsd

--- src/modules/statsd.c
+++ src/modules/statsd.c
@@ -399,7 +399,7 @@ static void report(void) {
 
 static int noit_statsd_init(noit_module_t *self) {
   unsigned short port = 8125;
-  int packets_per_cycle = 100;
+  int packets_per_cycle = 1000;
   int payload_len = 256*1024;
   struct sockaddr_in skaddr;
   int sockaddr_len;
