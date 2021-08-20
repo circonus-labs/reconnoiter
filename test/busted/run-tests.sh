d7653fd0b19a89d529d00d2347789f24c7697680
Eric Sproul <eric.sproul@circonus.com>
2021-08-20 16:46:14 -0400

    Fix run-tests for non-ASAN builds (#715)
    Invert ASAN check in run-tests
    
    The `-n` option might not be portable, and we're operating in
    traditional Bourne shell mode.
    
    Avoid setting any ASAN env if not building ASAN

--- test/busted/run-tests.sh
+++ test/busted/run-tests.sh
@@ -3,21 +3,23 @@
 PATH=$PATH:/opt/circonus/bin:/opt/pgsql9214/bin
 export PATH
 
-if [ -f /opt/llvm-5.0.0/bin/llvm-symbolizer ]
-then
-  ASAN_SYMBOLIZER_PATH=/opt/llvm-5.0.0/bin/llvm-symbolizer
-else
-  ASAN_SYMBOLIZER_PATH=/usr/bin/llvm-symbolizer
-fi
-
 DIR=`dirname $0`
 LD_LIBRARY_PATH=$DIR/../../src
-ASAN_OPTIONS="${ASAN_OPTIONS},log_path=${PWD}/${DIR}/asan.log"
-export LD_LIBRARY_PATH ASAN_SYMBOLIZER_PATH ASAN_OPTIONS
+export LD_LIBRARY_PATH
 
-if [ -n $BUILD_ASAN ]; then
+if [ -z "$BUILD_ASAN" ]; then
+  mtevbusted $@
+else
+  if [ -f /opt/llvm-5.0.0/bin/llvm-symbolizer ]
+  then
+    ASAN_SYMBOLIZER_PATH=/opt/llvm-5.0.0/bin/llvm-symbolizer
+  else
+    ASAN_SYMBOLIZER_PATH=/usr/bin/llvm-symbolizer
+  fi
+
+  ASAN_OPTIONS="${ASAN_OPTIONS},log_path=${PWD}/${DIR}/asan.log"
+  export ASAN_SYMBOLIZER_PATH ASAN_OPTIONS
+
   echo "Running tests with ASAN"
   mtevbusted-asan $@
-else
-  mtevbusted $@
 fi
