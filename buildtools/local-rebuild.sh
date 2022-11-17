a5da3f4c66ddcc63cdbda03d3adc9c56b794405a
vasuraman11 <36165091+vasuraman11@users.noreply.github.com>
2022-11-17 10:22:54 -0800

    Update and fix dev build and test scripts (#820)

--- buildtools/local-rebuild.sh
+++ buildtools/local-rebuild.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
-OS="$1"
-if test -z "${OS}" ; then
-    echo Usage: local-rebuild.sh '<OS>' '<optional_addl_make_switches>...' 1>&2
-    exit 1
+OS=`./buildtools/determine-os.sh local-rebuild.sh $@ 2>/dev/null`
+if [ "$?" != "0" ] ; then echo "OS detection error" ; exit 1 ; fi
+if [ "$OS" == "$1" ] ; then shift; fi;
+echo "Using OS: $OS"
+
+BUILD_INCLUDE_ENV=$1
+INCLUDE_ENV="./buildtools/$OS/env-$BUILD_INCLUDE_ENV.inc"
+if [ ! -f $INCLUDE_ENV ] ; then
+  BUILD_INCLUDE_ENV="normal"
+  INCLUDE_ENV="./buildtools/$OS/env.inc"
+  echo -n > .latest_buildtype
+else
+  shift
+  echo -n $BUILD_INCLUDE_ENV > .latest_buildtype
 fi
-shift
-source ./buildtools/$OS/env.inc
+
+ASAN_BUILD=""
+if [[ "$BUILD_INCLUDE_ENV" == "asan" ]] ; then
+  ASAN_BUILD=" with ASAN libs"
+  export BUILD_ASAN=1
+fi
+echo "Performing $BUILD_INCLUDE_ENV rebuild${ASAN_BUILD}..."
+source $INCLUDE_ENV
+
 autoreconf -i
 ./buildtools/${OS}/configure.sh
 EXIT_CODE="$?"
@@ -16,12 +33,23 @@ if [ "$EXIT_CODE" != "0" ] ; then
   echo "*************************"
   exit $EXIT_CODE
 fi
-./buildtools/build-clean.sh $@
+./buildtools/build-clean.sh EXTRA_CFLAGS="-Wall -Werror" EXTRA_CXXFLAGS="-Wall -Werror" $@
 EXIT_CODE="$?"
+
+echo
+grep -A 10 "Compile-time Configuration" config.log
+echo
+echo "Primary tooling:"
+grep -e "^gcc " config.log | head -n1
+grep -e "^g++ " config.log | head -n1
+grep -e "clang version" config.log | head -n1
+
+echo
+
 if [ "$EXIT_CODE" != "0" ] ; then
   echo "*********************"
   echo "*** Build failed! ***"
   echo "*********************"
 else
-  echo "Successfully completed local rebuild."
+  echo "Successfully completed $BUILD_INCLUDE_ENV rebuild${ASAN_BUILD}."
 fi
