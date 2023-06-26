5db9debf6640f6ed98a030380aaf9dd8c8ace164
Vasu Raman <vasu.raman@circonus.com>
2023-06-26 13:10:56 -0700

    busted test running under ASAN fixes

--- test/busted/run-tests.sh
+++ test/busted/run-tests.sh
@@ -11,18 +11,32 @@ if [[ -z "$BUILD_ASAN" && -z "$BUILD_UBSAN" ]]; then
   mtevbusted $@
 else
   if [[ -n "$BUILD_ASAN" ]]; then
-    if [[ -f /opt/llvm-5.0.0/bin/llvm-symbolizer ]]
-    then
-      ASAN_SYMBOLIZER_PATH=/opt/llvm-5.0.0/bin/llvm-symbolizer
-    else
-      ASAN_SYMBOLIZER_PATH=/usr/bin/llvm-symbolizer
+    if [[ -z "$ASAN_SYMBOLIZER_PATH" ]]; then
+      if [[ -r /etc/lsb-release ]]; then
+          . /etc/lsb-release
+          case $DISTRIB_RELEASE in
+              20.04)
+                  ASAN_SYMBOLIZER_PATH=`which llvm-symbolizer-14`
+                  ;;
+              22.04)
+                  ASAN_SYMBOLIZER_PATH=`which llvm-symbolizer-15`
+                  ;;
+          esac
+      fi
+    fi
+
+    # disable leak detection unless specifically enabled
+    if [[ "$LEAKS" != "1" ]]; then
+      ASAN_OPTIONS="detect_leaks=0,$ASAN_OPTIONS"
     fi
 
     ASAN_OPTIONS="${ASAN_OPTIONS},log_path=${PWD}/${DIR}/asan.log"
     export ASAN_SYMBOLIZER_PATH ASAN_OPTIONS
 
+    ASANRT_PATH=`./getasanlib.sh`
+
     echo "Running tests with ASAN"
-    mtevbusted-asan $@
+    LD_PRELOAD=$ASANRT_PATH mtevbusted $@
   elif [[ -n "$BUILD_UBSAN" ]]; then
     UBSAN_OPTIONS="${UBSAN_OPTIONS},log_path=${PWD}/${DIR}/ubsan.log"
     export UBSAN_OPTIONS
