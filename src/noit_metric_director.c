278e20b2e19db49f318c1c76fd47052da763ce1a
Philip Maddox <philip.maddox@circonus.com>
2025-03-06 21:14:40 +0000

    Deref, not ref

--- src/noit_metric_director.c
+++ src/noit_metric_director.c
@@ -1195,7 +1195,7 @@ handle_prometheus_message(const int64_t account_id,
     for (size_t j = 0; j < ts->n_samples; j++) {
       noit_metric_message_t *message = noit_prometheus_translate_to_noit_metric_message(&coercion,
         account_id, check_uuid, metric_name, ts->samples[j]);
-      noit_metric_director_message_ref(message);
+      noit_metric_director_message_deref(message);
     }
     // TODO: More handling
     free(metric_name);
